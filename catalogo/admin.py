a6b5608035ae82998a68000c6444dbb28cf14c28
Bruno Perdona <bruno15perdona@gmail.com>
2022-01-31 19:38:30 -0300

    Modelos registrados no painel admin, fixtures para testes

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,12 @@
+from tkinter import E
 from django.contrib import admin
 
 # Register your models here.
+
+from catalogo.models import Genero, Linguagem, Autor, Livro, ExemplarLivro
+
+admin.site.register(Genero)
+admin.site.register(Linguagem)
+admin.site.register(Autor)
+admin.site.register(Livro)
+admin.site.register(ExemplarLivro)
